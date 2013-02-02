476faa0fb9239cb08ef51b83832f3aa8e36aca60
git <git@pur-logic.biz>
2013-02-02 17:17:27 -0500

    init

---
+++ src/main/java/com/purlogic/ae/App.java
@@ -0,0 +1,13 @@
+package com.purlogic.ae;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
