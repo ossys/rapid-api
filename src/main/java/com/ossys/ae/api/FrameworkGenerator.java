3e334aaec3ebd335c70b34bc05e2b96011dd822c
ccravens <chad.cravens@ossys.com>
2015-03-30 20:27:37 -0400

    Added @Deprecated annotation to soon-to-be deprecated methods

--- src/main/java/com/ossys/ae/api/FrameworkGenerator.java
+++ src/main/java/com/ossys/ae/api/FrameworkGenerator.java
@@ -6,16 +6,27 @@ public interface FrameworkGenerator {
 	public boolean generateArtifacts();
 	public boolean addResources();
 	public String getDirectory(String name);
+	@Deprecated
 	public String getRoot();
+	@Deprecated
 	public String getIncludeRoot();
+	@Deprecated
 	public String getModelRoot();
+	@Deprecated
 	public String getEntitiesRoot();
+	@Deprecated
 	public String getExceptionsRoot();
+	@Deprecated
 	public String getUtilsRoot();
+	@Deprecated
 	public String getTestsRoot();
+	@Deprecated
 	public String getDbRoot();
+	@Deprecated
 	public String getDbLibRoot();
+	@Deprecated
 	public String getControllersRoot();
+	@Deprecated
 	public String getRouterRoot();
 	public String getModelNamespace(String namespace);
 	public String getEntityNamespace(String namespace);
