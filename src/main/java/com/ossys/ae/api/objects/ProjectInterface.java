7eb20c232224c591107c2934b4e1fe362b5cbd8a
ccravens <chad.cravens@ossys.com>
2015-04-26 00:24:08 -0400

    Updated API to better support single-project and multi-project systems

--- src/main/java/com/ossys/ae/api/objects/ProjectInterface.java
+++ src/main/java/com/ossys/ae/api/objects/ProjectInterface.java
@@ -7,6 +7,8 @@ public interface ProjectInterface {
 	public String getName();
 	@Deprecated
 	public String getLocation();
+	public String getDirectoryName();
+	public String getRootPath();
 	public List<DomainModelInterface> getDomainModels();
 	public void addDomainModel(DomainModelInterface model);
 	
--- src/main/java/com/ossys/ae/api/objects/SystemModelInterface.java
+++ src/main/java/com/ossys/ae/api/objects/SystemModelInterface.java
@@ -1,12 +1,12 @@
 package com.ossys.ae.api.objects;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 public interface SystemModelInterface {
 
-	public boolean exists();
 	public String getRootPath();
-	public void parse();
+	public void parse() throws FileNotFoundException;
 	public List<DataSourceInterface> getDatasources();
 	public List<ProjectInterface> getProjects();
 	public DataSourceInterface getDatasource(String name);
