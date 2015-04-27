7ef36cd71f2f4367d370c5a059f6ce136511b071
ccravens <chad.cravens@ossys.com>
2015-04-26 23:42:18 -0400

    Removed deprecated method in favor for the getRootPath() method

--- src/main/java/com/ossys/ae/api/objects/ProjectInterface.java
+++ src/main/java/com/ossys/ae/api/objects/ProjectInterface.java
@@ -5,8 +5,6 @@ import java.util.List;
 public interface ProjectInterface {
 	
 	public String getName();
-	@Deprecated
-	public String getLocation();
 	public String getDirectoryName();
 	public String getRootPath();
 	public List<DomainModelInterface> getDomainModels();
