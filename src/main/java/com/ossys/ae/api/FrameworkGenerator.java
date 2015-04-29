18f83614529fc007f5bf61d8225e7e6612b3645f
ccravens <chad.cravens@ossys.com>
2015-04-29 00:41:47 -0400

    Updated API to support API for individual location of resources rather than a generic getDirectory() method.

--- src/main/java/com/ossys/ae/api/FrameworkGenerator.java
+++ src/main/java/com/ossys/ae/api/FrameworkGenerator.java
@@ -5,29 +5,21 @@ public interface FrameworkGenerator {
 	public boolean generateDirectoryStructure();
 	public boolean generateArtifacts();
 	public boolean addResources();
+	
+	@Deprecated
 	public String getDirectory(String name);
-	@Deprecated
 	public String getRoot();
-	@Deprecated
 	public String getIncludeRoot();
-	@Deprecated
 	public String getModelRoot();
-	@Deprecated
 	public String getEntitiesRoot();
-	@Deprecated
 	public String getExceptionsRoot();
-	@Deprecated
 	public String getUtilsRoot();
-	@Deprecated
 	public String getTestsRoot();
-	@Deprecated
 	public String getDbRoot();
-	@Deprecated
 	public String getDbLibRoot();
-	@Deprecated
 	public String getControllersRoot();
-	@Deprecated
 	public String getRouterRoot();
+	
 	public String getModelNamespace(String namespace);
 	public String getEntityNamespace(String namespace);
 	public String getDBNamespace(String namespace);
