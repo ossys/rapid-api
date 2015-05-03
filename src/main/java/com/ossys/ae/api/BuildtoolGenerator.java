47c340f4290a36af0915a19ce93afe1b9f9a073a
ccravens <chad.cravens@ossys.com>
2015-05-02 22:59:25 -0400

    Added methods to BuildtoolGenerator interface

--- src/main/java/com/ossys/ae/api/BuildtoolGenerator.java
+++ src/main/java/com/ossys/ae/api/BuildtoolGenerator.java
@@ -6,4 +6,32 @@ public interface BuildtoolGenerator {
 	public boolean generateArtifacts();
 	public boolean addResources();
 	
+	public String getRoot();
+	
+	public String getSrcRoot();
+	public String getDbSrcRoot();
+	public String getEntitiesSrcRoot();
+	public String getExceptionsSrcRoot();
+	public String getUtilsSrcRoot();
+	public String getDbLibSrcRoot();
+
+	public String getIncludeRoot();
+	public String getDbIncludeRoot();
+	public String getEntitiesIncludeRoot();
+	public String getExceptionsIncludeRoot();
+	public String getUtilsIncludeRoot();
+
+	public String getTestRoot();
+	public String getTestSrcRoot();
+	public String getTestIncludeRoot();
+	
+	public String getControllersRoot();
+	public String getRouterRoot();
+	
+	public String getModelNamespace();
+	public String getEntityNamespace();
+	public String getDBNamespace();
+	public String getUtilsNamespace();
+	public String getExceptionsNamespace();
+	
 }
