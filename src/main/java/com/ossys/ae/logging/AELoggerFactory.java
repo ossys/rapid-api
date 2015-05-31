2f845c3965a31c7b0967ea26c2bb807d52754fa9
ccravens <chad.cravens@ossys.com>
2015-05-31 00:31:19 -0400

    Added logging flag checks

--- src/main/java/com/ossys/ae/logging/AELoggerFactory.java
+++ src/main/java/com/ossys/ae/logging/AELoggerFactory.java
@@ -2,10 +2,6 @@ package com.ossys.ae.logging;
 
 public class AELoggerFactory {
 	
-	public static AELogger getLogger(String canonical_name) {
-		return new AELogger(canonical_name);
-	}
-	
 	public static AELogger getLogger(Class<?> cls) {
 		return new AELogger(cls);
 	}
