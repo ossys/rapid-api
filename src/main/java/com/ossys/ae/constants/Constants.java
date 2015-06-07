db4bd2be6986fa75781e2a7fd93f2b83d57458ce
ccravens <chad.cravens@ossys.com>
2015-06-06 22:41:07 -0400

    Updated protocol from https to http when retrieving XML schema definition files

--- src/main/java/com/ossys/ae/constants/Constants.java
+++ src/main/java/com/ossys/ae/constants/Constants.java
@@ -4,8 +4,8 @@ public final class Constants {
 	public static final String NAME = "ActiveEngine.io";
 	public static final String VERSION = "0.2.0";
 	public static final String SYSTEM_MODEL = "ae/system.xml";
-	public static final String SYSTEM_MODEL_XSD = "https://ae.ossys.com/systemmodel.xsd";
-	public static final String DOMAIN_MODEL_XSD = "https://ae.ossys.com/domainmodel.xsd";
+	public static final String SYSTEM_MODEL_XSD = "http://ae.ossys.com/systemmodel.xsd";
+	public static final String DOMAIN_MODEL_XSD = "http://ae.ossys.com/domainmodel.xsd";
 
 	public static final String USER_DIR = System.getProperty("user.dir");
 	public static final String NEW_LINE = System.getProperty("line.separator");
