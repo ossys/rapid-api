5be4aaf18514bcc9b5e1fb9f8ac5871b62b3201c
ccravens <chad.cravens@ossys.com>
2015-09-15 15:01:56 -0400

    Updated to pull from AE schemas instead of local schemas

--- src/main/java/com/ossys/ae/constants/Constants.java
+++ src/main/java/com/ossys/ae/constants/Constants.java
@@ -4,12 +4,12 @@ public final class Constants {
 	public static final String NAME = "ActiveEngine.io";
 	public static final String VERSION = "0.2.0";
 	public static final String SYSTEM_MODEL = "ae/system.xml";
-//	public static final String SYSTEM_MODEL_XSD = "http://ae.ossys.com/systemmodel.xsd";
-	public static final String SYSTEM_MODEL_XSD = "file:///Users/ccravens/Business/ossys/Projects/AE/src/ae/ae-impl/src/main/resources/models/systemmodel.xsd";
-//	public static final String DOMAIN_MODEL_XSD = "http://ae.ossys.com/domainmodel.xsd";
-	public static final String DOMAIN_MODEL_XSD = "file:///Users/ccravens/Business/ossys/Projects/AE/src/ae/ae-impl/src/main/resources/models/domainmodel.xsd";
-//	public static final String INTERFACE_MODEL_XSD = "http://ae.ossys.com/interfacemodel.xsd";
-	public static final String INTERFACE_MODEL_XSD = "file://Users/ccravens/Business/ossys/Projects/AE/src/ae/ae-impl/src/main/resources/models/interfacemodel.xsd";
+	public static final String SYSTEM_MODEL_XSD = "http://ae.ossys.com/systemmodel.xsd";
+//	public static final String SYSTEM_MODEL_XSD = "file:///Users/ccravens/Business/ossys/Projects/AE/src/ae/ae-impl/src/main/resources/models/systemmodel.xsd";
+	public static final String DOMAIN_MODEL_XSD = "http://ae.ossys.com/domainmodel.xsd";
+//	public static final String DOMAIN_MODEL_XSD = "file:///Users/ccravens/Business/ossys/Projects/AE/src/ae/ae-impl/src/main/resources/models/domainmodel.xsd";
+	public static final String INTERFACE_MODEL_XSD = "http://ae.ossys.com/interfacemodel.xsd";
+//	public static final String INTERFACE_MODEL_XSD = "file://Users/ccravens/Business/ossys/Projects/AE/src/ae/ae-impl/src/main/resources/models/interfacemodel.xsd";
 
 	public static final String USER_DIR = System.getProperty("user.dir");
 	public static final String NEW_LINE = System.getProperty("line.separator");
