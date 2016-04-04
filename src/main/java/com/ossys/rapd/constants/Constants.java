123ce00863d2730923bcb9ef8dd7f30124c0aa8f
ccravens <chad.cravens@ossys.com>
2016-04-03 23:37:33 -0400

    Changed version numbers and removed final references to Active Engine

--- src/main/java/com/ossys/rapd/constants/Constants.java
+++ src/main/java/com/ossys/rapd/constants/Constants.java
@@ -2,7 +2,7 @@ package com.ossys.rapd.constants;
 
 public final class Constants {
 	public static final String NAME = "Rapd.io";
-	public static final String VERSION = "0.2.1";
+	public static final String VERSION = "1.0.0";
 	public static final String SYSTEM_MODEL = "rapd/system.xml";
 	public static final String SYSTEM_MODEL_XSD = "http://rapd.ossys.com/systemmodel.xsd";
 //	public static final String SYSTEM_MODEL_XSD = "file:///Users/ccravens/Business/ossys/Projects/Rapd/src/rapd/rapd-impl/src/main/resources/models/systemmodel.xsd";
